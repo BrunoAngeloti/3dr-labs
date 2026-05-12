fc0a7ec99f4fc26431bbdc109bf4422233f3e98c
Diego Ribeiro <143950847+Ribeirodi7@users.noreply.github.com>
2026-05-12 15:31:48 -0300

    Update catalog.ts

--- data/catalog.ts
+++ data/catalog.ts
@@ -33,11 +33,11 @@ export const catalog: Category[] = [
         description:
           "Inspirado em Como Treinar Seu Dragão, este Banguela em estilo crochê combina fofura e criatividade em uma peça única. Ideal para decorar mesas, nichos e setups, ou presentear fãs do dragão mais querido do cinema. Produzido em impressão 3D, é um item colecionável cheio de personalidade e charme.",
         images: [
-          "/produtos/Banguela-croche.jpg",
-          "/produtos/Banguela-croche2.jpg",
-          "/produtos/Banguela-croche3.jpg",
-          "/produtos/Banguela-croche4.jpg",
-          "/produtos/Banguela-croche5.jpg",
+          "/produtos/banguela-croche.jpg",
+          "/produtos/banguela-croche2.jpg",
+          "/produtos/banguela-croche3.jpg",
+          "/produtos/banguela-croche4.jpg",
+          "/produtos/banguela-croche5.jpg",
          ],
       },
       {
@@ -47,11 +47,11 @@ export const catalog: Category[] = [
         description:
           "Uma peça decorativa cheia de charme que une o visual delicado do crochê com a criatividade da impressão 3D. Perfeito para decorar mesas, nichos e estantes, ou presentear quem ama itens fofos, criativos e colecionáveis.",
         images: [
-          "/produtos/esquilo-croche.jpg",
-          "/produtos/esquilo-croche2.jpg",
-          "/produtos/esquilo-croche3.jpg",
-          "/produtos/esquilo-croche4.jpg",
-          "/produtos/esquilo-croche5.jpg",
+          "/produtos/Esquilo-croche.jpg",
+          "/produtos/Esquilo-croche2.jpg",
+          "/produtos/Esquilo-croche3.jpg",
+          "/produtos/Esquilo-croche4.jpg",
+          "/produtos/Esquilo-croche5.jpg",
         ],
       },
       {
