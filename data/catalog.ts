cfb7725508af7c6e5310e65ca3cdbedf8d5908a2
Diego Ribeiro <143950847+Ribeirodi7@users.noreply.github.com>
2026-09-09 18:42:04 -0300

    Update catalog.ts

--- data/catalog.ts
+++ data/catalog.ts
@@ -1530,7 +1530,7 @@ export const catalog: Category[] = [
   {    
     id: "pokecroche",
     title: "Pokemons Crochê",  
-    image: "/produtos/pokemom-croche/pikachu.png",
+    image: "/produtos/pokemom-croche/pikahcu.png",
     products: [
       {
         id: "caterpie",
@@ -1579,7 +1579,7 @@ export const catalog: Category[] = [
         description:
           "",
         images: [
-          "/produtos/pokemom-croche/pikachu.jpg",
+          "/produtos/pokemom-croche/pikahcu.jpg",
         ],
       },
       {
